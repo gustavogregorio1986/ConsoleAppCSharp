175d816110a2ba6678e8d9a85eee1a12b92169ed
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-05-27 17:25:32 -0300

    Add project files.

---
+++ Aula02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A mensagem.");
+        }
+    }
+}
