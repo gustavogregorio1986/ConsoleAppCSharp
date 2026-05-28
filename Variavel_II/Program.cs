f02f2db45d5e23289724ddeb10fa965157b1c9bb
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-05-28 17:13:38 -0300

    desenvolvendo tipo de dados em C#

---
+++ Variavel_II/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Variavel_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipo de dados no C#
+            /*
+             * inteiro 1, 2, 3
+             * ponto fliutuante -> 1.55, 1.90, -2000.2
+             * texto -> string "escrevermos dentro "
+             * 
+             * logica -> tur e ou false */
+
+            int numero = 1; // atribuimos valor
+            int x = 1 + 1; //atribuindo um expressão
+
+            double d = 1.1 + 5.50;
+            Console.WriteLine(d);
+
+            //int - número inteiros 
+            //double - números de ponto flutuante
+
+            double dd = 1; // 5 x 10 ^ 324 até 1.7 x 10 ^ 308
+            float f = 1.2f; //-3.4 x 10 ^38 até + 3.4 x 10 ^ 28
+
+            long l;
+            int a;
+
+            byte b; //0 - 255
+        }
+    }
+}
