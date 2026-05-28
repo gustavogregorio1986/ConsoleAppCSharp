ef2ebff8b7f74e2b399281fe6d6800b573d98b4c
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-05-28 14:36:34 -0300

    Desenvolvimento da logica de programação de comentario

---
+++ Aula03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula03  //Agrupador de instruções
+{
+    public class eXcript //Modelo de um objeto
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            if(num == 0)
+                Console.WriteLine("Ola mundo!");
+        }
+    }
+}
---
+++ Aula04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Isso aqui é um comentario
+
+            /*
+             * 
+             */
+
+            /*
+               
+             */
+        }
+    }
+}
