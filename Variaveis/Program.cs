4185b35c297fa38a89acbb4e7158a43ea5efd61d
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-05-28 16:21:34 -0300

    Desenvolvimento de logica de programação com variaveis

---
+++ Variaveis/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*==========                      =============
+ ===========   www.eXcript.com.br =============
+============                      =============*/
+
+namespace Variaveis
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Estudo das variáveis
+            /*
+             * Variável é um local na memória que reservamos
+             * para armazenar um valor
+             *     1) Armazenar o nome de um Cliente
+             *     2) Armazenar a quantidade de um produto no estoque
+             *     3) Armazenar a hora do sistema 
+             *     4) Armazenadr o usuário que está logado */
+
+            int num = 200;
+            Console.WriteLine(num);
+
+            bool b = true;
+
+            string s = "Aqui eu posso escrever textos";
+            Console.WriteLine(b);
+            Console.WriteLine(s);
+
+        }
+    }
+}
