c83cd3e9f7f80c2a0635d0ad97cee11e1e1a7860
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-05-29 17:36:25 -0300

    Desenvolvendo logica de programação com variaveis

---
+++ Variaveis_III/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Variaveis_III
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Caracteriatica e Estrutura de uma Variável
+            /*
+             * 1) Nomre
+             * 2) Tipo 
+             * 3) Tamanho
+             * 4) Valor
+             * 
+             * 1) armazenamento é temporario
+             * 2) os dados sempre serão removido da memoria
+             * 3) os dados contidos nas variaveis são volateis
+             * 4) o valor contido numa variavel é sobrescrito pelo novo valor
+             * 5) o C# desaloca a mémoria automatica
+             */
+
+            int numTotal;
+            int _e = 1;
+             
+        }
+    }
+}
