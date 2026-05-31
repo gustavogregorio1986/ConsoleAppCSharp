d15701a8db6a638fafa624ff835b80234ea41495
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-05-31 17:08:58 -0300

    Desenvolvimento de logica de programação com ler do teclado

---
+++ LendoOTeclado/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LendoOTeclado
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lendo o tevclado
+
+            string texto; //declarando a variável que recebrá a string dgita
+
+            Console.WriteLine("Digite o texto");
+            texto = Console.ReadLine();  //ler o usuário digita
+            Console.WriteLine("O valor digitado foi: " + texto);
+            Console.WriteLine(texto);
+        }
+    }
+}
