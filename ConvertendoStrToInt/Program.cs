9adc2e556143268cf6ce8770ca6f590635775ff4
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-06-01 14:45:11 -0300

    Desenvolvimento da conversão de texto em numero na logica de programação

---
+++ ConvertendoStrToInt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConvertendoStrToInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Convertendo texto em numeros
+
+            string s = "10000";
+            int i;
+
+            i = int.Parse(s);
+
+            int x = i + 10; // 10 + 10
+            Console.WriteLine(x);
+
+        }
+    }
+}
