e6f97f1996143f00f08549bfcd37499588ee3a9e
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-06-01 16:58:42 -0300

    desenvolvimento da tomada de decisão com if na logica de programação

---
+++ Aula/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 0;
+
+            //Console.WriteLine("Digite um numero: ");
+            //string str = Console.ReadLine();
+            //num = Convert.ToInt32(str);
+
+            //if(num > 0)
+            //{
+            //    Console.WriteLine("O numero digitado é positivo");
+            //}
+
+            int num = 0;
+            string texto = "O número digoitado é: ";
+            Console.WriteLine("Digite um número qualquer: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if(num > 0)
+                Console.WriteLine(texto + " positivo");
+             if(num < 0)
+                Console.WriteLine(texto + "negativo"); 
+            }
+        }
+    }
+
